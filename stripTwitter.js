882f9f28315973b37cf9035dba1dc5975de0b39b
Aaron Cohen <ioncohen2@gmail.com>
2024-12-24 21:30:23 -0500

    StripTwitter now removes the for you infinite scroll

--- stripTwitter.js
+++ stripTwitter.js
@@ -14,4 +14,20 @@ function  stripTwitter(){
     //remove follow suggestions
     const whoToFollow = document.querySelector('[aria-label="Who to follow"]');
     hide(whoToFollow);
+
+    //find timeline home
+    const homeTimeline = document.querySelector('[aria-label="Timeline: Your Home Timeline"]');
+
+    //find "for you" span using xpath
+    const spanList = document.getElementsByTagName('span');
+    for(var i = 0; i < spanList.length; i++){
+        if (spanList[i].innerHTML === 'For you'){
+            if (spanList[i].parentNode.parentNode.parentNode.getAttribute('aria-selected')==='true'){
+                hide(homeTimeline);
+                return;
+            }
+        }
+    }
+    homeTimeline.style.display = 'flex';
+
 }
